959458ad0f2693861279a5bd7787267c7a0ac74c
George <George@DESKTOP-VEE7U8H>
2016-09-28 15:50:00 -0700

    Add successfully passing tests

---
+++ StackOverflow.Tests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using StackOverflow.Controllers;
+using StackOverflow.Models;
+using Xunit;
+
+namespace StackOverflow.Tests
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ StackOverflow.Tests/QuestionTest.cs
@@ -0,0 +1,22 @@
+﻿using StackOverflow.Models;
+using Xunit;
+
+namespace StackOverflow.Tests
+{
+    public class ItemTest
+    {
+        [Fact]
+        public void GetTitleTest()
+        {
+            //Arrange
+            var question = new Question();
+            question.Title = "Wash the dog?";
+
+            //Act
+            var result = question.Title;
+
+            //Assert
+            Assert.Equal("Wash the dog?", result);
+        }
+    }
+}
